6485e00c60ebc1ab70ff4eab6dbe95c2c73b86fb
Gabriel Peçanha Diniz <gabrielpdiniz02@gmail.com>
2024-09-27 16:37:23 -0300

    Add files via upload

---
+++ aula pratica 2/aula02/src/Caneta.java
@@ -0,0 +1,33 @@
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+
+    public void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Esta tampada? " + this.tampada);
+        System.out.println("Carga: " + this.carga);
+    }
+
+    public void rabiscar() {
+        if (this.tampada == true){
+            System.out.println("ERRO! Nao posso rabiscar");
+        } else {
+            System.out.println("Estou rabiscando");
+        }
+
+    }
+
+    public void tampar() {
+        this.tampada = true;
+        
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+}
